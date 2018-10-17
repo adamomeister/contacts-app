95b3500f713a3070dbdd89db8b1fc08d389fabf7
CHRIS ADAMO <adamomeister@gmail.com>
2018-10-16 19:16:46 -0500

    create index and show actions using RESTful CRUD

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,14 @@
+class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render "index.json.jbuilder"
+  end
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render "show.json.jbuilder"
+  end
+  def create
+    
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # STEP 1: A ROUTE triggers a controller action
+  # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get '/contacts' => 'contacts#index'
+    get '/contacts/:id' => 'contacts#show'
+    post 'contacts' => 'contacts#create'
+  end
+end
